4eeda561947d49294f5c052e4df3f8da2b143533
thao nguyen <phong5363@gmail.com>
2025-06-03 20:13:24 +0700

    mang

--- 030625.c
+++ 030625.c
@@ -1,20 +1,17 @@
-// 030625.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
 
-#include <iostream>
+int main() {
 
-int main()
-{
-    std::cout << "Hello World!\n";
+	int arr[10];
+	int sum = 0;
+	printf("nhap so nguyen: \n");
+	for (int i = 0; i < 10; i++) {
+		printf("Phan tu %d: ", i + 1);
+		scanf("%d", &arr[i]);
+		sum += arr[i];
+	}
+	printf("tong: %d\n", sum);
+	return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
